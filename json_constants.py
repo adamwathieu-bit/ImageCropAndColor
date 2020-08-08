3fdd3323d075eaebff073a90fe1d5d9e2b1ea2c6
Adam Wathieu <ajw135@georgetown.edu>
2020-08-08 00:50:53 -0400

    adjusted to fit greater tunability of JSON file

--- json_constants.py
+++ json_constants.py
@@ -5,3 +5,7 @@ CROPPED_TIFF_DIRECTORY = "path_to_save_cropped_tiffs"
 CIRCLE_MIN_DIST = "min_dist_between_circles"
 CIRCLE_MIN_RADIUS = "min_radius_of_circles"
 HOUGH_DP = "dp_index"
+CIRCLE_WIDTH_BUFFER = "circle_width_buffer"
+
+COLOR_RANGE = "color_range"
+COLOR_CHANNELS = "color_channels"
